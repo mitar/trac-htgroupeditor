2634f320bc7f2869c431076c4d453e64ec137f14
Mitar <mitar.git@tnode.com>
2013-03-27 22:50:56 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='TracHtGroupEditorPlugin',
-    version='2.0',
+    version='2.0.1',
     author = 'Robert Martin',
     author_email = 'robert.martin@arqiva.com',
     description = 'HT Group Editor plugin for Trac',
